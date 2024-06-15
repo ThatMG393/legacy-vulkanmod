8530fe7cd0aa9a1cb4d77912a5f7eabde7b5c4b8
thatmg393 <thatmg393@gmail.com>
2024-06-15 15:07:56 +0000

    remove cus found new workaround

--- src/main/java/com/thatmg393/legacyvkm/mixins/client/MinecraftClientM.java
+++ src/main/java/com/thatmg393/legacyvkm/mixins/client/MinecraftClientM.java
@@ -20,35 +20,6 @@ import net.minecraft.client.MinecraftClient;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientM {
-    @Inject(method = "setPixelFormat", at = @At("INVOKE"))
-    private void setPixelFormat_HEAD(CallbackInfo ci) throws LWJGLException {
-        LegacyVulkanMod.LOGGER.info("Patchy time? No? Alright :(");
-
-        // TODO: MOVE TO LEGACY LWJGL3 (or not cus platform specific issue)
-        ClassPool pool = new ClassPool();
-        pool.appendClassPath(new LoaderClassPath(FabricLauncherBase.getLauncher().getTargetClassLoader()));
-
-        try {
-            CtClass cls = pool.get("org.lwjgl.glfw.GLFW");
-            
-            try {
-                cls.getDeclaredMethod("glfwPlatformSupported", new CtClass[] { CtClass.intType });
-                LegacyVulkanMod.LOGGER.info("seems like no patch needed.");
-            } catch (Exception e1) {
-                LegacyVulkanMod.LOGGER.info("punjabiloonchir detected!1!1!");
-
-                CtMethod mth = new CtMethod(CtClass.booleanType, "glfwPlatformSupported", new CtClass[] { CtClass.intType }, cls);
-                mth.setBody("return true;");
-
-                cls.addMethod(mth);
-                // DefineClassHelper.toClass(cls.getName(), null, pool.getClassLoader(), null, cls.toBytecode());
-                cls.detach();
-            }
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
-        }
-    }
-
     @Inject(method = "setPixelFormat", at = @At("RETURN"))
     private void setPixelFormat_RETURN(CallbackInfo ci) throws LWJGLException {
         LegacyVulkanMod.LOGGER.info("Window was probably created.");
