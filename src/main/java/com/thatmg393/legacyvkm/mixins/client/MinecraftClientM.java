b15dbfb80fa03130121293c87dadb47edecf425d
thatmg393 <thatmg393@gmail.com>
2024-06-15 13:59:39 +0000

    i am stupid with mixins

--- src/main/java/com/thatmg393/legacyvkm/mixins/client/MinecraftClientM.java
+++ src/main/java/com/thatmg393/legacyvkm/mixins/client/MinecraftClientM.java
@@ -1,7 +1,8 @@
 package com.thatmg393.legacyvkm.mixins.client;
 
+import java.io.IOException;
+
 import org.lwjgl.LWJGLException;
-import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,17 +15,18 @@ import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
 import javassist.LoaderClassPath;
+
 import net.fabricmc.loader.impl.launch.FabricLauncherBase;
 import net.minecraft.client.MinecraftClient;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientM {
     @Inject(method = "initializeGame", at = @At("HEAD"))
-    private void onInitializeGame() {
+    private void onInitializeGame(CallbackInfo ci) throws LWJGLException, IOException {
         // TODO: MOVE TO LEGACY LWJGL3 (or not cus platform specific issue)
         ClassPool pool = new ClassPool();
         pool.appendClassPath(new LoaderClassPath(FabricLauncherBase.getLauncher().getTargetClassLoader()));
-        
+
         try {
             CtClass cls = pool.get("org.lwjgl.glfw.GLFW");
             if (cls.getDeclaredMethod("glfwPlatformSupported", new CtClass[] { CtClass.intType }) == null) {
